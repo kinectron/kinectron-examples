e13d1a1182fe47bc95afe8b7a44c6bf57567171b
lisajamhoury <lisajamhoury@gmail.com>
2018-07-02 11:08:27 -0400

    Add recorded data to skeleton example

--- examples_p5js/skeleton/js/sketch.js
+++ examples_p5js/skeleton/js/sketch.js
@@ -1,28 +1,66 @@
-let myCanvas = null;
+// Set to true if using live kinectron data
+var liveData = false;
 
-// declare kinectron
-let kinectron = null;
+// Fill in Kinectron IP Address here ie. "127.16.231.33"
+var kinectronIpAddress = ""; 
+http://alpha.editor.p5js.org/lisajamhoury/sketches/SkJm1w58M
+// p5 canvas 
+var myCanvas = null;
+
+// Declare kinectron 
+var kinectron = null;
 
 // drawHand variables
-let start = 30;
-let target = 100;
-let diameter = start;
-let light = 255;
-let dark = 100;
-let hueValue = light;
-let lerpAmt = 0.3;
-let state = 'ascending';
+var start = 30;
+var target = 100;
+var diameter = start;
+var light = 255;
+var dark = 100;
+var hueValue = light;
+var lerpAmt = 0.3;
+var state = 'ascending';
+
+// recorded data variables
+var sentTime = Date.now();
+var currentFrame = 0;
+
 
 function setup() {
   myCanvas = createCanvas(500, 500);
   background(0);
   noStroke();
 
+  if (liveData) initKinectron();
+}
+
+
+function draw() {
+
+  if (!liveData) loopRecordedData();
+}
+
+
+function loopRecordedData() {
+  
+  // send data every 20 seconds 
+  if (Date.now() > sentTime + 20) {
+    bodyTracked(recorded_skeleton[currentFrame])
+    sentTime = Date.now();
+
+    if (currentFrame < recorded_skeleton.length-1) {
+      currentFrame++;
+    } else {
+      currentFrame = 0;
+    }
+  }
+
+}
+
+function initKinectron() {
   // Define and create an instance of kinectron
-  let kinectronIpAddress = ""; // FILL IN YOUR KINECTRON IP ADDRESS HERE
   kinectron = new Kinectron(kinectronIpAddress);
 
-  // Connect to the microstudio
+  // Connect to the ITP microstudio when live
   //kinectron = new Kinectron("kinectron.itp.tsoa.nyu.edu");
 
   // Connect with application over peer
@@ -30,20 +68,39 @@ function setup() {
 
   // Request all tracked bodies and pass data to your callback
   kinectron.startTrackedBodies(bodyTracked);
-}
-
-function draw() {
 
 }
+
 
 function bodyTracked(body) {
   background(0, 20);
 
-  // Get all the joints off the tracked body and do something with them
-  kinectron.getJoints(drawJoint);
+  var hands = [];
 
-  // Get the hands off the tracked body and do somethign with them
-  kinectron.getHands(drawHands);
+  // Get all the joints off the tracked body and do something with them
+  for(var jointType in body.joints) {
+    joint = body.joints[jointType];
+
+    drawJoint(joint);
+
+    // Get the hands off the tracked body and do somethign with them
+    
+    // Find right hand 
+    if (jointType == 11) {
+      hands.rightHand = joint;
+      hands.rightHandState = translateHandState(body.rightHandState);
+    }
+
+    // Find left hand
+    if (jointType == 7) {
+      hands.leftHand = joint;
+      hands.leftHandState = translateHandState(body.leftHandState);
+    }
+
+  }
+
+  drawHands(hands);
+
 }
 
 // Draw skeleton
@@ -59,22 +116,45 @@ function drawJoint(joint) {
   ellipse(joint.depthX * myCanvas.width, joint.depthY * myCanvas.height, 3, 3);
 }
 
+// Make handstate more readable
+function translateHandState(handState) {
+  switch (handState) {
+    case 0:
+      return 'unknown';
+
+    case 1:
+      return 'notTracked';
+
+    case 2:
+      return 'open';
+
+    case 3:
+      return 'closed';
+
+    case 4:
+      return 'lasso';
+  }
+}
+
+
 // Draw hands
 function drawHands(hands) {
 
-  //check if hands are touching
+  //check if hands are touching 
   if ((Math.abs(hands.leftHand.depthX - hands.rightHand.depthX) < 0.01) && (Math.abs(hands.leftHand.depthY - hands.rightHand.depthY) < 0.01)) {
     hands.leftHandState = 'clapping';
     hands.rightHandState = 'clapping';
   }
 
   // draw hand states
+  
   updateHandState(hands.leftHandState, hands.leftHand);
   updateHandState(hands.rightHandState, hands.rightHand);
 }
 
 // Find out state of hands
 function updateHandState(handState, hand) {
+
   switch (handState) {
     case 'closed':
       drawHand(hand, 1, 255);
