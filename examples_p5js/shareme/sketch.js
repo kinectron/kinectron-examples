391c2232f0d5d1254da70c68996a316beb10d9be
lisajamhoury <lisajamhoury@gmail.com>
2018-07-02 23:18:33 -0400

    Add share example draft

---
+++ examples_p5js/shareme/sketch.js
@@ -0,0 +1,62 @@
+var myCanvas = null;
+
+// Declare kinectron 
+var kinectron = null;
+
+var frameP;
+
+function setup() {
+  myCanvas = createCanvas(500, 500);
+  background(0);
+
+  frameP = createP('');
+}
+
+function initKinectron(e) {
+  e.preventDefault();
+
+  //Define and create an instance of kinectron
+  var kinectronIpAddress = document.getElementById('ip_address').value;
+  console.log('Connecting to ip ' + kinectronIpAddress);
+  kinectron = new Kinectron(kinectronIpAddress);
+
+  // Connect to the microstudio
+  //kinectron = new Kinectron("kinectron.itp.tsoa.nyu.edu");
+
+  // Connect with application over peer
+  kinectron.makeConnection();
+
+  // Set callbacks
+  kinectron.setColorCallback(drawFeed);
+  kinectron.setDepthCallback(drawFeed);
+  kinectron.setInfraredCallback(drawFeed);
+
+}
+
+function draw() {
+  var fps = frameRate();
+  fill(0);
+  stroke(0);
+  text("FPS: " + fps.toFixed(0), 10, height);
+  frameP.html(fps.toFixed(0));
+}
+
+// Choose camera to start based on key pressed
+function keyPressed() {
+  if (keyCode === ENTER) {
+    kinectron.startColor();
+  } else if (keyCode === UP_ARROW) {
+    kinectron.startDepth();
+  } else if (keyCode === DOWN_ARROW) {
+    kinectron.startInfrared();
+  } else if (keyCode === RIGHT_ARROW) {
+    kinectron.stopAll();
+  }
+}
+
+function drawFeed(img) {
+  // Draws feed using p5 load and display image functions  
+  loadImage(img.src, function(loadedImage) {
+    image(loadedImage, 0, 0);
+  });
+}
